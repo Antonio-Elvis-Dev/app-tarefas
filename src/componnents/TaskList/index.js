c288ba1c55e8974f02a30ab330a7ae09362909e1
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2022-10-15 18:31:58 -0300

    update

--- src/componnents/TaskList/index.js
+++ src/componnents/TaskList/index.js
@@ -3,8 +3,23 @@ import { View, Text, StyleSheet } from "react-native";
 
 export default function TaskList({data}){ // desconstruindo a props e chamando a data tireto
     return(
-        <View>
-            <Text>{data.nome}</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Ola mundo</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:"row",
+        backgroundColor:'#121212',
+        alignItems:"center",
+        marginBottom:10,
+        padding:10,
+        borderRadius:4
+    },
+    text:{
+        color:'#fff'
+    }
+})
